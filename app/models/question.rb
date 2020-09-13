e89b41b5c9a1ddb68ad222f745958eb8b9e6583a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-13 22:42:41 +0200

    add additional associations to user and question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
-    belongs_to :user
+    belongs_to :creator, class_name: "User", foreign_key: "user_id"
+    has_many :users
     has_many :answers
     has_many :comments, through: :answers
+
 end
