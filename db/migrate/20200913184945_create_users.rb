754f2b3098bef8b7d786d94521b9ec3471922014
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-13 20:59:06 +0200

    setting up basic migrations

---
+++ db/migrate/20200913184945_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :location
+      t.string :fields
+      t.integer :upvotes, default: 0
+      t.integer :downvotes, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200913185224_create_questions.rb
@@ -0,0 +1,14 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.string :content
+      t.integer :user_id
+      t.string :jurisdiction
+      t.string :field
+
+      t.timestamps
+
+    end
+  end
+end
