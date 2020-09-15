d53fd02c90ae2eb990408ef09dc93422b1cd0ba7
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-15 02:12:47 +0200

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :followings
     has_many :followed_questions, through: :followings, source: :question
     has_many :created_questions, foreign_key: :creator_id, class_name: 'Question'
     has_many :answers
     has_many :comments
+
+    validates :username, uniqueness: { case_sensitive: false }
 end
