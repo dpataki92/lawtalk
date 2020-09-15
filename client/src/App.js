f5b680b611640554b6d29415334acfdd015bfb98
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-15 22:59:24 +0200

    setting up first version of login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./components/login"
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('api/users', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  },
-  body: JSON.stringify({
-    user: {
-      username: "Pataki",
-      password: "trialpassword",
-      location: "Hungary",
-      avatar: "default.jpg",
-      fields: "tax"
-    }
-  })
-})
-  .then(r => r.json())
-  .then(function(json) {
-    console.log(json)
-  })
-    }
   
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-  
+        <Login />
       </div>
     );
   }
