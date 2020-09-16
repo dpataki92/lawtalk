99fe3658152ae7bf1548f53a446fdadfc4928f01
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-16 15:58:23 +0200

    setting up login page html and  style with bootstrap template

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -5,46 +5,49 @@ import '../App.css';
 const Login = props => {
     return (
         <div className="limiter">
-            <div className="container-login100" style={{backgroundImage: 'public/login_background/1.jpeg'}}>
-                <div className="wrap-login100 p-t-190 p-b-30">
-                <form className="login100-form validate-form">
-                <span className="login100-form-title p-t-20 p-b-45">
-                Welcome to Lawtalk
-                </span>
-                <div className="wrap-input100 validate-input m-b-10" data-validate="Username is required">
-                <input className="input100" type="text" name="username" placeholder="Username"/>
-                <span className="focus-input100"></span>
-                <span className="symbol-input100">
-                <i className="fa fa-user"></i>
-                </span>
-                </div>
-                <div className="wrap-input100 validate-input m-b-10" data-validate="Password is required">
-                <input className="input100" type="password" name="pass" placeholder="Password"/>
-                <span className="focus-input100"></span>
-                <span className="symbol-input100">
-                <i className="fa fa-lock"></i>
-                </span>
-                </div>
-                <div className="container-login100-form-btn p-t-10">
-                <button className="login100-form-btn">
-                Login
-                </button>
-                </div>
-                <div className="text-center w-full p-t-25 p-b-230">
-                <a href="#" className="txt1">
-                Forgot Username / Password?
-                </a>
-                </div>
-                <div className="text-center w-full">
-                <a className="txt1" href="#">
-                Create new account
-                <i className="fa fa-long-arrow-right"></i>
-                </a>
-                </div>
-                </form>
-                </div>
-                </div>
-            </div>
+		<div className="container-login100">
+			<div className="wrap-login100">
+				<form className="login100-form validate-form">
+					<span className="login100-form-title p-b-34">
+						Account Login
+					</span>
+					
+					<div className="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
+						<input id="first-name" className="input100" type="text" name="username" placeholder="User name"/>
+						<span className="focus-input100"></span>
+					</div>
+					<div className="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
+						<input className="input100" type="password" name="pass" placeholder="Password"/>
+						<span className="focus-input100"></span>
+					</div>
+					
+					<div className="container-login100-form-btn">
+						<button className="login100-form-btn">
+							Sign in
+						</button>
+					</div>
+
+					<div className="w-full text-center p-t-27 p-b-239">
+						<span className="txt1">
+							Forgot
+						</span>
+
+						<a href="#" className="txt2">
+							User name / password?
+						</a>
+					</div>
+
+					<div className="w-full text-center">
+						<a href="#" className="txt3">
+							Sign Up
+						</a>
+					</div>
+				</form>
+
+				<div className="login100-more" style={{backgroundImage: `url("/login_background/1.jpg")`}}></div>
+			</div>
+		</div>
+	</div>
     )
 }
 
