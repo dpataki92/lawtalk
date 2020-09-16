3427844571d1b94a5215e942671e17599a3db5a3
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-16 16:04:42 +0200

    updating html for login page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -9,11 +9,11 @@ const Login = props => {
 			<div className="wrap-login100">
 				<form className="login100-form validate-form">
 					<span className="login100-form-title p-b-34">
-						Account Login
+						Lawtalk
 					</span>
 					
 					<div className="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
-						<input id="first-name" className="input100" type="text" name="username" placeholder="User name"/>
+						<input id="first-name" className="input100" type="text" name="username" placeholder="Username"/>
 						<span className="focus-input100"></span>
 					</div>
 					<div className="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
@@ -29,13 +29,12 @@ const Login = props => {
 
 					<div className="w-full text-center p-t-27 p-b-239">
 						<span className="txt1">
-							Forgot
+							or login with social media account
 						</span>
 
-						<a href="#" className="txt2">
-							User name / password?
-						</a>
 					</div>
+
+
 
 					<div className="w-full text-center">
 						<a href="#" className="txt3">
