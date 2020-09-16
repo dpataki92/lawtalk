42b53ee52a3c4c1c527a377047b29532630830a2
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-16 18:07:41 +0200

    adding state and onchange handlers for login form

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../App.css'; 
+import Home from './home';
 
 
 const Login = props => {
+	state = {
+		username:  '',
+		password: ''
+	}
+
+	handleOnChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
     return (
-        <div className="limiter">
+        <div className="limiter" id="login-form">
 		<div className="container-login100">
 			<div className="wrap-login100">
 				<form className="login100-form validate-form">
@@ -13,11 +25,11 @@ const Login = props => {
 					</span>
 					
 					<div className="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
-						<input id="first-name" className="input100" type="text" name="username" placeholder="Username"/>
+						<input id="first-name" className="input100" type="text" name="username" placeholder="Username" required onChange={e=> this.handleOnChange(e)} value={this.state.username}/>
 						<span className="focus-input100"></span>
 					</div>
 					<div className="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
-						<input className="input100" type="password" name="pass" placeholder="Password"/>
+						<input className="input100" type="password" name="pass" placeholder="Password" required onChange={e=> this.handleOnChange(e)} value={this.state.password}/>
 						<span className="focus-input100"></span>
 					</div>
 					
@@ -33,8 +45,6 @@ const Login = props => {
 						</span>
 
 					</div>
-
-
 
 					<div className="w-full text-center">
 						<a href="#" className="txt3">
