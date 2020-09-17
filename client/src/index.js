be57efa2e3d188667b856c3782b8210d40145594
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-17 18:01:15 +0200

    setting up initial user reducer and action file

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import usersReducer from './reducers/users.js'
 
-const users = () => []
+
 const reducer = combineReducers({
-
+  user: usersReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
