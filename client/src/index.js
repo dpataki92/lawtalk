4fd3f683a28b933b2646da9ef854185a32fa2200
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-17 18:05:51 +0200

    restrucuting code for setting up store

--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import usersReducer from './reducers/users.js'
+import store from './store';
 
-
-const reducer = combineReducers({
-  user: usersReducer
-})
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 ReactDOM.render(
   <Provider store={store}>
     <App />
