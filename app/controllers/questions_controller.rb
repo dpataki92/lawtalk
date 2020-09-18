da7fe55b51ab2ac0721634ab47c8c273892b0bfd
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-18 22:27:48 +0200

    setting up question serializer and basic index method for listing questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,6 @@
 class QuestionsController < ApplicationController
+    def index
+        questions = Quuestion.all.relevant[0..6]
+        render json: QuestionSerializer.new(questions).to_serialized_json 
+    end
 end
---
+++ app/serializers/question_serializer.rb
@@ -0,0 +1,14 @@
+class QuestionSerializer < ActiveModel::Serializer
+    def initialize(question_object)
+        @question = question_object
+      end
+      
+      def to_serialized_json
+        @question.to_json(:include => {
+          :answers,
+          :comments,
+          :followers,
+          :creator
+        })
+      end
+end
