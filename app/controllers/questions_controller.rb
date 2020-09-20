3ce73cdd10c6a4cc8c0111f8d5a68a56ae690403
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-20 23:00:37 +0200

    debugging ordering logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,7 @@
 class QuestionsController < ApplicationController
     def index
-        sorted = Question.sorted_questions(params[:field], params[:jurisdiction], params[:searchWord])
-        ordered = sorted.ordered_questions(params[:order])
-        cut_questions = Question.page_cut(ordered, params[:pageNumber])
+        sorted_and_ordered = Question.ordered_questions(params[:order]).sorted_questions(params[:field], params[:jurisdiction], params[:searchWord])
+        cut_questions = Question.page_cut(sorted_and_ordered, params[:pageNumber])
         render json: {questions: Question.questions_data_for_list(cut_questions)} 
     end
 end
