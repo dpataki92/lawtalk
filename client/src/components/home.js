8239708915581ef4ceb9889832d366a0a9f9296a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-21 22:56:08 +0200

    adding function for handling left and right scroll of page numbers

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -134,6 +134,23 @@ class Home extends React.Component {
         } 
     }
 
+    handleScroll = (e) => {
+        e.preventDefault();
+        if (e.target.id === "scrollLeft" && !e.target.classList.contains("disabled")) {
+            document.querySelectorAll(".pageNum").forEach((el) => {
+                el.innerText = parseInt(el.innerText) - 3
+            })
+            e.target.parentNode.classList += " disabled";
+            document.getElementById("scrollRight").parentNode.classList = "page-item";
+        } else if (e.target.id === "scrollRight" && !e.target.classList.contains("disabled")) {
+            document.querySelectorAll(".pageNum").forEach((el) => {
+                el.innerText = parseInt(el.innerText) + 3
+            })
+            e.target.parentNode.classList += " disabled";
+            document.getElementById("scrollLeft").parentNode.classList = "page-item";
+        }
+    }
+
     render() {
 
         return(
@@ -170,11 +187,11 @@ class Home extends React.Component {
     <QuestionList questions={this.props.questions}/>
     <nav>
         <ul className="pagination mb-5">
-            <li className="page-item disabled"><a className="page-link" href="javascript:void(0)" data-abc="true">«</a></li>
-            <li className="page-item active" id="firstPage"><a className="page-link" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>1</a></li>
-            <li className="page-item"><a className="page-link" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>2</a></li>
-            <li className="page-item"><a className="page-link" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>3</a></li>
-            <li className="page-item"><a className="page-link" href="javascript:void(0)" data-abc="true">»</a></li>
+            <li className="page-item disabled" ><a onClick={this.handleScroll} id="scrollLeft" className="page-link" href="javascript:void(0)" data-abc="true">«</a></li>
+            <li className="page-item active" id="firstPage"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>1</a></li>
+            <li className="page-item"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>2</a></li>
+            <li className="page-item"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={this.handlePageChangeOnEnter} onMouseLeave={this.handlePageChangeOnLeave} onClick={this.getQuestionsBasedOnPageNumber}>3</a></li>
+            <li className="page-item" ><a onClick={this.handleScroll} id="scrollRight" className="page-link" href="javascript:void(0)" data-abc="true">»</a></li>
         </ul>
     </nav>
     
