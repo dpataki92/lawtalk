4f0aed944f92e795a639a33d4789d8a97d1acbaf
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-22 01:42:28 +0200

    adding page reload function to navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -8,6 +8,10 @@ const Navbar = props => {
         props.logout();
     }
 
+    const handleReload = () => {
+        window.location.reload(false);
+    }
+
     return(
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid"> <button class="navbar-toggler navbar-toggler-right border-0 p-0" type="button" data-toggle="collapse" data-target="#navbar20">
@@ -19,7 +23,7 @@ const Navbar = props => {
                         <li class="nav-item"> <a class="nav-link" href="#" onMouseDown={props.setFollowedToTrue} onMouseUp={props.handleSearchClick}>My questions</a> </li>
                         <li class="nav-item"> <a class="nav-link" href="#">Top users</a> </li>
                     </ul>
-                    <p class="d-none d-md-block lead mb-0 text-white"> <i class="fa d-inline fa-lg fa-stop-circle"></i> <b> LAWTALK</b> </p>
+                    <p class="d-none d-md-block lead mb-0 text-white"> <i class="fa d-inline fa-lg fa-stop-circle"></i> <b onClick={handleReload}> LAWTALK</b> </p>
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item mx-1"> <a class="nav-link" href="#" > <i class="fa fa-edit fa-fw fa-lg"></i> </a> </li>
                         <li class="nav-item mx-1"> <a class="nav-link" href="#" onClick={handleLogout}> <i class="fa fa-sign-out fa-fw fa-lg"></i> </a> </li>
