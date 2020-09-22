d18e2f824bea889acf4b2040a00feebfadd7376a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-22 22:14:09 +0200

    adding placeholders to Thread component

--- client/src/components/thread.js
+++ client/src/components/thread.js
@@ -4,8 +4,8 @@ class Thread extends Component {
     render(){
         return(
             <React.Fragment>
-                <MainPost/>
-                <Replies />
+                <MainPost avatar={this.props.question.avatar} creation={this.props.question.creation} username={this.props.question.username} content={this.props.content} jurisdiction={this.props.question.jurisdiction}/>
+                <Replies replies={this.props.question.replies}/>
             </React.Fragment>
         )
     }
