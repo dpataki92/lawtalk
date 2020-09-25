06a353cb3b68e92cfb3341375030bf3cf2bf4c7c
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-25 16:57:39 +0200

    connecting thread component to redux, adjusting props

--- client/src/components/thread.js
+++ client/src/components/thread.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import MainPost from './mainPost.js';
+import Replies from './replies.js';
+import { connect } from 'react-redux';
+import { getCurrentThread } from '../actions/currentThread';
 
 class Thread extends Component {
     render(){
+        const { questionData } = this.props.currentThread
+        const { answerData } = this.props.currentThread
+
         return(
             <React.Fragment>
-                <MainPost avatar={this.props.question.avatar} creation={this.props.question.creation} username={this.props.question.username} content={this.props.content} jurisdiction={this.props.question.jurisdiction} answer={answer}/>
-                <Replies replies={this.props.question.replies}/>
+                <MainPost avatar={questionData.creator.avatar} creation={questionData.created_at.slice(0, -14)} username={questionData.creator.username} content={questionData.content} jurisdiction={questionData.jurisdiction}/>
+                <Replies replies={answerData}/>
             </React.Fragment>
         )
     }
 
 }
 
-export default Thread;
+const mapStateToProps = state => {
+    return ({ currentThread: state.currentThread })
+  }
+
+export default connect(mapStateToProps, { getCurrentThread })(Thread);
