7cfa2fb7c89717cd0de05e96281000d6946c09d8
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-26 21:16:30 +0200

    adding styles to searchbar buttons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -17,7 +17,7 @@ const Navbar = props => {
         <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid"> <button className="navbar-toggler navbar-toggler-right border-0 p-0" type="button" data-toggle="collapse" data-target="#navbar20">
-                    <p className="navbar-brand text-white mb-0"> <i className="fa d-inline fa-lg fa-stop-circle"></i> LAWTALK </p>
+                    <p className="navbar-brand text-white mb-0"> LAWTALK </p>
                 </button>
                 <div className="collapse navbar-collapse" id="navbar20">
                     <ul className="navbar-nav mr-auto">
@@ -25,7 +25,7 @@ const Navbar = props => {
                         <li className="nav-item"> <NavLink className="nav-link" to="/questions/followed">My questions</NavLink> </li>
                         <li className="nav-item"> <NavLink className="nav-link" to="/users/top">Top users</NavLink> </li>
                     </ul>
-                    <p className="d-none d-md-block lead mb-0 text-white"> <i className="fa d-inline fa-lg fa-stop-circle"></i> <b onClick={handleReload}> LAWTALK</b> </p>
+                    <p className="d-none d-md-block lead mb-0 text-white"> <b onClick={handleReload}> LAWTALK</b> </p>
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item mx-1"> <a className="nav-link" href="#" > <i className="fa fa-edit fa-fw fa-lg"></i> </a> </li>
                         <li className="nav-item mx-1"> <a className="nav-link" href="#" onClick={handleLogout}> <i className="fa fa-sign-out fa-fw fa-lg"></i> </a> </li>
--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
             <div style={{marginLeft: "12px", marginRight:"12px"}}>
             <div className="d-flex flex-wrap justify-content-between">
                 <div> <Link to="/questions/new"> <button type="button" className="btn btn-shadow btn-wide btn-primary">  <span className="btn-icon-wrapper pr-2 opacity-7"> <i className="fa fa-plus fa-w-20"></i> </span> New question </button> </Link> </div>
-                <button id="order" onMouseEnter={this.props.handleOrderOnEnter} onMouseLeave={this.props.handleOrderOnLeave} onClick={this.props.ordering}>{`${"Relevance ↓" }`}</button>
+                <button className="search-button" id="order" onMouseEnter={this.props.handleOrderOnEnter} onMouseLeave={this.props.handleOrderOnLeave} onClick={this.props.ordering}>{`${"Relevance ↓" }`}</button>
             </div>
             <div className="d-flex flex-wrap justify-content-between" style={{marginTop:"5px"}}>
                 <div className="col-12 col-md-3 p-0 mb-3"> 
@@ -20,11 +20,10 @@ class SearchBar extends Component {
                     {<FieldList setField={this.props.setField}/>} 
                 </div>
                 <div className="col-12 col-md-3 p-0 mb-3" id="jurisdiction"> 
-                        <button id="EU" className="jurisdictionList" onClick={this.props.handleJurisdictionButton} style={{marginRight:"5px"}}>EU</button>
-                        OR 
-                        <button id="USA" className="jurisdictionList" onClick={this.props.handleJurisdictionButton} style={{marginLeft:"5px"}}>USA</button>
+                        <button id="EU" className="jurisdictionList search-button" onClick={this.props.handleJurisdictionButton} style={{marginRight:"5px"}}>EU</button> 
+                        <button id="USA" className="jurisdictionList search-button" onClick={this.props.handleJurisdictionButton} style={{marginLeft:"5px"}}>USA</button>
                         {<JursidictionList category={this.props.jurisdictionCategory} setJurisdiction={this.props.setJurisdiction}/>}</div>
-                <button onMouseDown={this.props.setPageBackToOne} onMouseUp={this.props.handleSearchClick}>Search</button>
+                <button className="search-button" onMouseDown={this.props.setPageBackToOne} onMouseUp={this.props.handleSearchClick}>Search</button>
             </div>
             </div>
             </React.Fragment>
