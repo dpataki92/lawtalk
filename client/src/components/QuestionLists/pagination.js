e8c7dfdb00a5184e9d12f184b09ab78be88a3dd5
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-10-06 19:10:32 +0200

    refactoring pagination component

--- client/src/components/QuestionLists/pagination.js
+++ client/src/components/QuestionLists/pagination.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Pagination = props => {
     
@@ -24,9 +26,9 @@ const Pagination = props => {
             <ul className="pagination mb-5">
                 <li className="page-item disabled" ><a onClick={handleScroll} id="scrollLeft" className="page-link" href="javascript:void(0)" data-abc="true">«</a></li>
                 <li className="page-item active" id="firstPage"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={props.handlePageChangeOnEnter} onMouseLeave={props.handlePageChangeOnLeave} onClick={props.getQuestionsBasedOnPageNumber}>1</a></li>
-                <li className="page-item"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={props.handlePageChangeOnEnter} onMouseLeave={props.handlePageChangeOnLeave} onClick={props.getQuestionsBasedOnPageNumber}>2</a></li>
-                <li className="page-item"><a className="page-link pageNum" href="javascript:void(0)" data-abc="true" onMouseEnter={props.handlePageChangeOnEnter} onMouseLeave={props.handlePageChangeOnLeave} onClick={props.getQuestionsBasedOnPageNumber}>3</a></li>
-                <li className="page-item" ><a onClick={handleScroll} id="scrollRight" className="page-link" href="javascript:void(0)" data-abc="true">»</a></li>
+                <li className="page-item"><Link className="page-link pageNum" to=" " data-abc="true" onMouseEnter={props.handlePageChangeOnEnter} onMouseLeave={props.handlePageChangeOnLeave} onClick={props.getQuestionsBasedOnPageNumber}>2</Link></li>
+                <li className="page-item"><Link className="page-link pageNum" to=" " data-abc="true" onMouseEnter={props.handlePageChangeOnEnter} onMouseLeave={props.handlePageChangeOnLeave} onClick={props.getQuestionsBasedOnPageNumber}>3</Link></li>
+                <li className="page-item" ><Link onClick={handleScroll} id="scrollRight" className="page-link" to=" " data-abc="true">»</Link></li>
             </ul>
         </nav>
     )
