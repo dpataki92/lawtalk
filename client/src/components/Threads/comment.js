f245cb5a85640371d037ae8889c4e0714a40672d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-10-06 19:23:39 +0200

    refactoring comment component

--- client/src/components/Threads/comment.js
+++ client/src/components/Threads/comment.js
@@ -9,8 +9,8 @@ const Comment = (props) => {
             <p><Link to={`/users/${props.commentCreatorId}`}>{props.commentCreator}</Link></p> 
             <p style={{opacity: "0.5"}}>{props.creation}</p><br/>
             <p>{props.content}</p><br/>
-            <a style={{cursor: "pointer"}} id="upvote"> <i className="fa fa-arrow-up text-success" style={{fontSize: "1.3em"}}></i> </a>  <span id="upvoteNum" style={{opacity: "0.5", marginRight:"5px"}} >{props.upvotes} upvotes</span> 
-            <a style={{cursor: "pointer"}} id="downvote"> <i className="fa fa-arrow-down text-danger" style={{fontSize: "1.3em"}}></i> </a>  <span id="downvoteNum" style={{opacity: "0.5", marginRight:"5px"}}>{props.downvotes} downvotes</span>
+            <Link to=" " style={{cursor: "pointer"}} id="upvote"> <i className="fa fa-arrow-up text-success" style={{fontSize: "1.3em"}}></i> </Link>  <span id="upvoteNum" style={{opacity: "0.5", marginRight:"5px"}} >{props.upvotes} upvotes</span> 
+            <Link to=" " style={{cursor: "pointer"}} id="downvote"> <i className="fa fa-arrow-down text-danger" style={{fontSize: "1.3em"}}></i> </Link>  <span id="downvoteNum" style={{opacity: "0.5", marginRight:"5px"}}>{props.downvotes} downvotes</span>
             <hr className="m-0"/><br/>
         </div>
     )
