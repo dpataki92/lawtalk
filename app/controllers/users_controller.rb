c38d335bddcdd6716948193d04aa62c4bc123b96
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-10-19 22:04:42 +0200

    debugging signup logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.password = params[:password]
+    @user.avatar = 'default.jpg'
     @user.save
     
     if @user.valid?
