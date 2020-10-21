ca1dadd1cb0609beddb15b51bce737e2bb663151
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-10-22 00:16:17 +0200

    refactoring QuestionRow component

--- client/src/components/QuestionLists/questionRow.js
+++ client/src/components/QuestionLists/questionRow.js
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 
 
 class QuestionRow extends React.Component {
-    
+
     state = {
-        vote: '',
         voteCounter: 0
     }
 
@@ -23,37 +22,37 @@ class QuestionRow extends React.Component {
         e.preventDefault();
         const vote = e.target.name;
         this.setState({
-            vote: vote,
-            voteCounter: vote === "upvote" ? 1  : 0
+            voteCounter: vote === "upvote" ? 1 : 0
         })
     }
 
     render() {
-    const props = this.props
-    return(
-        <div className="card-body py-3">
-            <div className="row no-gutters align-items-center">
-                <div className="col"> <Link to={`/questions/${props.questionId}`} className="text-big" data-abc="true">{props.title}</Link> <span className={`badge badge-${this.userConnection()[1]} align-text-bottom ml-1`}>{this.userConnection()[0]}</span>
-                    <div className="text-muted small mt-1" >{`${props.jurisdiction},`} {props.field} &nbsp;·&nbsp; <Link to={`/users/${props.id}`} className="text-muted" data-abc="true">{props.creator}</Link></div>
-                    <div>
-                        <button className="search-button" onClick={this.handleVote} style={{padding: "2px 8px"}} name="upvote">↑</button>
-                        <button className="search-button" onClick={this.handleVote} style={{padding: "2px 8px"}} name="downvote">↓</button>
-                        <strong> Votes: {this.state.voteCounter}</strong>
+        const props = this.props
+        return(
+            <div className="card-body py-3">
+                <div className="row no-gutters align-items-center">
+                    <div className="col"> <Link to={`/questions/${props.questionId}`} className="text-big" data-abc="true">{props.title}</Link> <span className={`badge badge-${this.userConnection()[1]} align-text-bottom ml-1`}>{this.userConnection()[0]}</span>
+                        <div className="text-muted small mt-1" >{`${props.jurisdiction},`} {props.field} &nbsp;·&nbsp; <Link to={`/users/${props.id}`} className="text-muted" data-abc="true">{props.creator}</Link></div>
+                        <div>
+                            <button className="search-button" onClick={this.handleVote} style={{padding: "2px 8px"}} name="upvote">↑</button>
+                            <button className="search-button" onClick={this.handleVote} style={{padding: "2px 8px"}} name="downvote">↓</button>
+                            <strong> Votes: {this.state.voteCounter}</strong>
+                        </div>
                     </div>
-                </div>
-                <div className="d-none d-md-block col-4">
-                    <div className="row no-gutters align-items-center">
-                    <div className="col-4">{props.replies}</div>
-                        <div className="media col-8 align-items-center"> <img src={`/${props.lastRespondentAvatar}`} alt="last respondent's profile picture" className="d-block ui-w-30 rounded-circle" style={{width: "70px", height: "80px"}}/>
-                            <div className="media-body flex-truncate ml-2">
-                            <div className="line-height-1 text-truncate">{props.lastResponseDate}</div> {props.lastRespondent === "-" ? "-" : <Link to={`/users/${props.lastRespondentId}`} className="text-muted small text-truncate" data-abc="true">{props.lastRespondent}</Link>} 
+                    <div className="d-none d-md-block col-4">
+                        <div className="row no-gutters align-items-center">
+                        <div className="col-4">{props.replies}</div>
+                            <div className="media col-8 align-items-center"> <img src={`/${props.lastRespondentAvatar}`} alt="last respondent's profile picture" className="d-block ui-w-30 rounded-circle" style={{width: "70px", height: "80px"}}/>
+                                <div className="media-body flex-truncate ml-2">
+                                <div className="line-height-1 text-truncate">{props.lastResponseDate}</div> {props.lastRespondent === "-" ? "-" : <Link to={`/users/${props.lastRespondentId}`} className="text-muted small text-truncate" data-abc="true">{props.lastRespondent}</Link>} 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )}
+        )
+    }
 }
 
 export default QuestionRow;
