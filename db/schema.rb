ebce49d2d2f5e81c71ec26f8477b0a0aa06f5235
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-09 21:39:52 +0100

    add CreateVotes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_193039) do
+ActiveRecord::Schema.define(version: 2020_11_09_203800) do
 
   create_table "answers", force: :cascade do |t|
     t.string "content"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 2020_09_18_193039) do
     t.string "bio"
     t.integer "upvotes", default: 0
     t.integer "downvotes", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "answer_id"
+    t.integer "user_id"
+    t.integer "comment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
