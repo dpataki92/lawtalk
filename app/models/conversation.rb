72f64636ad2070fbb74b6e23522dd983b548e4a3
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-13 22:33:25 +0100

    add validation to Conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,7 @@
 class Conversation < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :receiver, class_name: 'User'
+
+    validates :author, uniqueness: {scope: :receiver}
+
 end
