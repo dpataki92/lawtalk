178c0830ddd5672d2f35a8fbe9e137b45ecc0031
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-14 22:43:48 +0100

    make chat list row appear as active on click

--- client/src/components/Users/chatList.js
+++ client/src/components/Users/chatList.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import ChatListItem from './chatListItem.js'
 
 class ChatList extends Component {
+
+    handleClick = (e) => {
+        document.querySelector(".active_chat").classList.remove("active_chat");
+        e.target.closest(".chat_list").classList.add("active_chat");
+    }
+
     render() {
         let chatlist = this.props.chatlist.map((c, id)=> {
             return (
-                    <div className={`chat_list ${id === 0 ? 'active_chat' : null}`}> 
+                    <div className={`chat_list ${id === 0 ? 'active_chat' : ""}`} onClick={this.handleClick}> 
                     <ChatListItem
                         key={id}
                         conversationId={c.id}
