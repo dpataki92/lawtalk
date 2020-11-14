612d221221e690122ba7901fd3176b8cf6cdc25b
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-14 18:06:47 +0100

    add conversations serializer method

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,4 +6,21 @@ class Conversation < ApplicationRecord
 
     validates :author, uniqueness: {scope: :receiver}
 
+    def self.conversations_serializer(user)
+        result = []
+        
+        conversations = (user.authored_conversations + user.received_conversations).sort {|a,b| b.updated_at <=> a.updated_at}
+
+        conversations.each do |c|
+            chatPartner = c.author === user ? c.receiver : c.author
+            result.push({
+                lastMessageDate: Date::ABBR_MONTHNAMES[c.updated_at.month] + " " + c.updated_at.day.to_s,
+                lastMessage: c.messages.sort {|a,b| b.created_at <=> a.created_at}[0].content[0..77],
+                chatPartner: chatPartner.username
+            })
+        end
+
+        result
+    end
+
 end
