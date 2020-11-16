ec165ffba7742f3e26b7511cf37a27519fff7648
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-16 20:26:25 +0100

    include add_message controller action to add message to conversations based on user input

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,4 +9,15 @@ class ConversationsController < ApplicationController
         render json: {currentConversation: conversation.messages_serializer(current_user)}
     end
 
+    def add_message
+        conversation = Conversation.find_by(id: params[:convId])
+        user = User.find_by(id: params[:id])
+        if params[:message]
+            conversation.messages.create(user: user, content: params[:message])
+            render json: {message: "success"}
+        else
+            render json: {message: "failure"}
+        end
+    end
+
 end
