413ccad462e14b07cf238146f4ff01d552c91eb3
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-18 14:28:17 +0100

    set up initial authentication flow with Auth0

--- client/src/components/Account/socialMediaContainer.js
+++ client/src/components/Account/socialMediaContainer.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loginSocialMedia } from '../../actions/currentUser.js';
+import { useAuth0} from '@auth0/auth0-react';
 
+const SocialMediaContainer = () => {
 
-class SocialMediaContainer extends React.Component {
+        const { loginWithRedirect } = useAuth0();
+        const { user } = useAuth0();
 
-    handleClick = (e) => {
-        if (e.target.className.slice(6) === "google") {this.props.loginSocialMedia("google_oath2")}
-        else if (e.target.className.slice(6) === "twitter") {this.props.loginSocialMedia("twitter")}
-    }
+        console.log(user)
 
-    render() {
         return(
                 <div className="w-full text-center p-t-10 p-b-100">
 						<span className="txt1">
 							or login with social media account
 						</span><br/><br/>
-						<a  href="#" onClick={this.handleClick} className="fa fa-google"></a>
-						<a  href="#" onClick={this.handleClick} className="fa fa-twitter"></a>
+						<a  href="" onClick={() => loginWithRedirect()} className="fa fa-google"></a>
+						<a  href="" onClick={() => loginWithRedirect()} className="fa fa-twitter"></a>
 				</div>
         )
-    }
 }
 
-export default connect(null, { loginSocialMedia })(SocialMediaContainer);
+export default SocialMediaContainer;
