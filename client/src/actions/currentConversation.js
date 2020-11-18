2963128c7e1a450fdbfb633d6439a06e7871df7a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-11-18 18:24:50 +0100

    set redux action to start conversation and set new conversation as current

--- client/src/actions/currentConversation.js
+++ client/src/actions/currentConversation.js
@@ -26,3 +26,27 @@ export const getCurrentConversation = (id, convId) => {
         })
     }
 }
+
+// asynchronous action creators
+export const startConversation = (userId) => {
+    return dispatch => {
+        return fetch(`/api/conversations/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem('jwt_token')}`
+            },
+            body: JSON.stringify({
+                userId: userId
+            })
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            if (json.currentConversation) {
+                dispatch(setCurrentConversation(json.currentConversation))
+            } else {
+                dispatch(setCurrentConversation({}))
+            }           
+        })
+    }
+}
